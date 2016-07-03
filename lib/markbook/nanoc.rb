f0ad57e540b66c70c13a1656d06e8806c88e4658
Denis Defreyne <denis.defreyne@stoneship.org>
2016-07-03 22:52:54 +0200

    Fix style

--- lib/markbook/nanoc.rb
+++ lib/markbook/nanoc.rb
@@ -3,7 +3,7 @@ require 'markbook'
 Class.new(Nanoc::Filter) do
   identifier :markbook2html
 
-  def run(content, params = {})
+  def run(content, _params = {})
     MarkBook.to_html(content)
   end
 end
